67cadce0c8ae0b8ffef1328aac267ab7d68c2ba2
leejaefriend <leejh6237@gmail.com>
2025-03-12 00:59:26 +0900

    CH 6 MC 완료

--- CH 6 MC.py
+++ CH 6 MC.py
@@ -85,4 +85,53 @@ class QAgent():
             action = np.argmax(action_val)
         return action
 
+    def update_table(self, history):
+        # 한 에피소드에 해당하는 history를 입력으로 받아 q 테이블의 값을 업데이트 한다
+        cum_reward = 0
+        for transition in history[::-1]:
+            s, a, r, s_prime = transition
+            x, y = s
+            # 몬테카를로 방식을 이용하여 업데이트
+            self.q_table[x, y, a] = self.q_table[x, y, a] + self.alpha * (cum_reward - self.q_table[x, y, a])
+            cum_reward = cum_reward + r
+
+    def anneal_eps(self):
+        self.eps -= 0.03
+        self.eps = max(self.eps, 0.1)
+
+    def show_table(self):
+        # 학습이 각 위치에서 어느 액션의 q 값이 가장 높았는지 보여주는 함수
+        q_lst = self.q_table.tolist()
+        data = np.zeros((5, 7))
+        for row_idx in range(len(q_lst)):
+            row = q_lst[row_idx]
+            for col_idx in range(len(row)):
+                col = row[col_idx]
+                action = np.argmax(col)
+                data[row_idx, col_idx] = action
+        print(data)
+
+def main():
+    env = GridWorld()
+    agent = QAgent()
+
+    for n_epi in range(1000): # 총 1,000 에피소드 동안 학습
+        done = False
+        history = []
+
+        s = env.reset()
+        while not done: # 한 에피소드가 끝날 때까지
+            a = agent.select_action(s)
+            s_prime, r, done = env.step(a)
+            history.append((s, a, r, s_prime))
+            s = s_prime
+        agent.update_table(history) # 히스토리를 이용하여 에이전트를 업데이트
+        agent.anneal_eps()
+
+    agent.show_table() # 학습이 끝난 결과를 출력
+
+if __name__ == '__main__':
+    main()
+
+
 
